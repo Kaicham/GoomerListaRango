3f5f196aabd2efb0135fbd10ac0d9d861ef26943
Kaique Santana <kaikham@hotmail.com>
2019-12-12 22:11:05 -0300

    Conectando com o banco de dados

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,25 @@ const server = express();
 
 const port = 8000
 
+//Connectando ao banco de dados - refatorar e separar. Utilizar função assincrona
+const mongoose = require('mongoose')
+const dataBaseURI = "mongodb+srv://admin:admin@cluster0-k77ap.mongodb.net/test?retryWrites=true&w=majority"
+
+mongoose.connect(dataBaseURI, { useNewUrlParser: true, useUnifiedTopology: true }, error => {
+    if(error){
+        console.log(error)
+        
+    }
+})
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB conectado com sucesso!')
+})
+
+//Connectando ao banco de dados - refatorar e separar. Utilizar função assincrona
+
+
 server.get('/', (req, res) => {
     console.log("Desafio iniciado!")
     res.send("Desafio iniciado!")
